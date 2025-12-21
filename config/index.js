8e21e8f521fe3cb707ec7d98fbebf2e7c3c09843
adam <a2e56b85@gmail.com>
2025-12-21 13:04:35 +0800

    why

--- config/index.js
+++ config/index.js
@@ -8,6 +8,9 @@ export const config = {
     process.env.JWT_SECRET || "your-super-secret-jwt-key-change-in-production",
   jwtExpire: process.env.JWT_EXPIRE || "7d",
   nodeEnv: process.env.NODE_ENV || "development",
-  corsOrigin: process.env.CORS_ORIGIN || "http://localhost:5173",
+  corsOrigin:
+    process.env.CORS_ORIGIN ||
+    "https://junkhub.vercel.app" ||
+    "http://localhost:5173",
   databaseUrl: process.env.DATABASE_URL,
 };
